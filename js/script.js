4ff26510683b52d2d48a76781350afa39105efd1
githubolga <myolga17@gmail.com>
2020-06-24 21:34:36 +0300

    js форма

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var buttonOpen = document.querySelector(".js-modal-open");
+var modal = document.querySelector(".js-modal");
+var buttonClose = modal.querySelector(".js-modal-close");
+var modalForm = modal.querySelector(".js-form");
+var formName = modalForm.querySelector(".js-name");
+var formEmail = modalForm.querySelector(".js-email");
+var formText = modalForm.querySelector(".js-text");
+
+buttonOpen.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.add("md-show");
+});
+
+buttonClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+ 	if (modal.classList.contains("md-show")) {
+		modal.classList.remove("md-show");
+	}
+});
+
+modalForm.addEventListener("submit", function (evt) {
+  if (!formName.value || !formEmail.value || !formText.value) {
+    evt.preventDefault(); 
+    modal.classList.add("md-error");
+    setTimeout(() => {
+    	console.log(123);
+    	modal.classList.remove("md-error"), 2000
+    });
+  } 
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modal.classList.contains("md-show")) {
+      evt.preventDefault();
+      modal.classList.remove("md-show");
+      // loginPopup.classList.remove("md-error");
+    }
+  }
+});
+
